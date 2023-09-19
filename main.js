51715d6c4679bb82524710a9ff46d9b2aeca98de
henry gad <henrygad.orji@gamil.com>
2023-09-19 18:43:50 +0100

    version 3

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-console.log("this is version 2 from henry loveday")
+console.log("this is version 2 from henry loveday");
+console.log('version 3')
+console.log('my git skill is improving for the better in version 3')
